fa68e47a548e2d6ed16e795b4f40c2ec0b874908
Minsub Song <minsub1031@gmail.com>
2023-04-24 10:20:10 +0900

    2023-04-24

---
+++ ecmaJS/timerPromise.mjs
@@ -0,0 +1,10 @@
+import { setTimeout, setInterval } from 'timers/promises';
+
+await setTimeout(3000);
+console.log('3초 뒤 실행');
+
+for await (const startTime of setInterval(1000, Date.now())) {
+    console.log(startTime);
+    console.log('1초마다 실행', new Date(startTime));
+}
+
